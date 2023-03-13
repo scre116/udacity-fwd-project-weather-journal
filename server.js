9ec21bc78e89b5e4c7cadfc97b0f6bd1511294f0
Eugen Schreiner <eugen.schreiner@bertelsmann.de>
2023-03-13 21:45:16 +0100

    setup server, add GET and POST routes

--- server.js
+++ server.js
@@ -1,19 +1,52 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross-origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+app.listen(port, function () {
+    console.log(`running on localhost: ${port}`);
+});
+
+// GET route
+app.get('/all', function (request, response) {
+    console.log('GET all');
+    response.send(projectData);
+});
+
+// POST route
+// request example:
+// {
+//     "date": "2020-01-01",
+//     "temperature": 20,
+//     "feelings": "I am feeling good"
+// }
+app.post('/add', function (request, response) {
+    console.log('POST add');
+    projectData['date'] = request.body.date;
+    projectData['temperature'] = request.body.temperature;
+    projectData['feelings'] = request.body.feelings;
+    console.log(projectData);
+    response.send("OK");
+
+});
+
